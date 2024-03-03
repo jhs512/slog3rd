2345f6bdbbb73d3c8a2f09054103dfe07ea14ad0
jhs512 <jangka512@gmail.com>
2024-03-03 20:50:51 +0900

    work

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,15 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <div className="font-thin">안녕</div>
-      <div className="font-extralight">안녕</div>
-      <div className="font-light">안녕</div>
-      <div className="font-normal">안녕</div>
-      <div className="font-medium">안녕</div>
-      <div className="font-semibold">안녕</div>
-      <div className="font-bold">안녕</div>
-      <div className="font-extrabold">안녕</div>
-      <div className="font-black">안녕</div>
+      <Button className="font-thin">안녕</Button>
+      <Button className="font-extralight">안녕</Button>
+      <Button className="font-light">안녕</Button>
+      <Button className="font-normal">안녕</Button>
+      <Button className="font-medium">안녕</Button>
+      <Button className="font-semibold">안녕</Button>
+      <Button className="font-bold">안녕</Button>
+      <Button className="font-extrabold">안녕</Button>
+      <Button className="font-black">안녕</Button>
     </main>
   );
 }
