1aa0584b1b4881afef52425b6951f450d9dea81c
jhs512 <jangka512@gmail.com>
2024-03-03 20:10:55 +0900

    work

---
+++ src/main/java/com/ll/slog3rd/Slog3rdApplication.java
@@ -0,0 +1,13 @@
+package com.ll.slog3rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slog3rdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slog3rdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/slog3rd/Slog3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.slog3rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slog3rdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
