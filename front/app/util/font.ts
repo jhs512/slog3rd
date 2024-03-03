0857388dbc675ef1fd8bc44658886d2b34090129
jhs512 <jangka512@gmail.com>
2024-03-03 20:46:45 +0900

    work

---
+++ front/app/util/font.ts
@@ -0,0 +1,8 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+  src: "../../public/fonts/Pretendard/PretendardVariable.woff2",
+  weight: "45 920",
+  style: "normal",
+  display: "swap",
+});
